0edbd4aabeeabc384bd0cbe0f0ab96f8bc3b58de
Jordan Bean <jorbea@live.unc.edu>
2022-11-01 21:48:34 -0400

    Completed roll.js and cli.js

---
+++ bin/cli.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import { roll } from "../lib/roll.js"
+import minimist from 'minimist';
+
+const args = minimist(process.argv.slice(2));
+
+export function main() {
+	console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls)));
+};
+
+main();
